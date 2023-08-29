032d2fa4a51f395324822a5d93c06bb891d460f5
Emilly Agatha <135267138+emyzou@users.noreply.github.com>
2023-08-29 15:54:50 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,69 @@
+//Emilly Agatha
+
+//Exercio 1
+/*
+a.
+b. null
+11
+indefinido
+indefinido
+indefinido
+
+//Exercicio 2
+SUBI NUM ONIBUS EM MIRROCOS
+27
+
+//B.1
+let nome = prompt("Digite seu nome.");
+let email = prompt("Digite seu email");
+
+console.log(`O e-mail ${email} foi cadastrado com sucesso. Seja bem-vinda(o) ${nome}!`);
+
+//2. 
+let comidasFavoritas = ["lasanha", "estrogonofe", "sushi", "churrasco", "sorvete"];
+
+console.log(comidasFavoritas);
+console.log(" Essas são minhas comidas favoritas")
+console.log(comidasFavoritas[0]);
+console.log(comidasFavoritas[1]);
+console.log(comidasFavoritas[2]);
+console.log(comidasFavoritas[3]);
+console.log(comidasFavoritas[4]);
+
+
+let comidaUsuario = prompt("Qual a sua comida favorita?");
+console.log(comidasFavoritas);
+comidasFavoritas.splice(1,1);
+comidasFavoritas[1] = comidaUsuario;
+console.log(comidasFavoritas);
+-----------------------//-----------------------------
+(forma simplificada)
+comidasFavoritas[1] = prompt("Qual a sua comida favorita?")
+ console.log(comidasFavoritas)
+
+//3.
+let listaDeTarefas = [];
+let tarefas = prompt("Cite uma tarefa que você realiza no dia a dia.");
+let tarefas2 = prompt("Cite outra tarefa que você realiza no dia a dia");
+let tarefas3 = prompt("Cite mais uma tarefa que você realiza no dia a dia");
+
+listaDeTarefas.push(tarefas, tarefas2, tarefas3);
+console.log(listaDeTarefas);
+
+listaDeTarefas.splice(Number(prompt("Diga qual tarefa ja realizou 0, 1 ou 2 ?")),1);
+console.log(listaDeTarefas);
+
+// desafios
+1.
+let frase = prompt("digite uma frase").toString();
+let f = frase.split(" ");
+console.log(f);
+
+2. 
+let frutas = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"];
+let f = frutas[2];
+console.log(frutas[2]);
+console.log(frutas.length);
+
+*/
+
